983571fbe5c24d86ec937c0ee3e50d312489e903
Shadhir Fawz <it22237118@my.sliit.lk>
2025-07-22 13:48:29 +0530

    DB config done

---
+++ Backend/src/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+import connectDB from "./config/db.js";
+
+// Routes
+import authRoutes from "./routes/authRoutes.js";
+import examRoutes from "./routes/examRoutes.js";
+// import questionRoutes from "./routes/questionRoutes.js";
+import resultRoutes from "./routes/resultRoutes.js";
+
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+connectDB();
+
+// Mount routes
+app.use("/api/auth", authRoutes);
+app.use("/api/exams", examRoutes);
+// app.use("/api/questions", questionRoutes);
+app.use("/api/results", resultRoutes);
+
+export default app;
